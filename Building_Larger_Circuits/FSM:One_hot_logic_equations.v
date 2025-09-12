fcb2c7a11d57eb5b8d359660fd4c1a191f5fdbc1
N S Devika <nsdevika2000@gmail.com>
2025-09-12 10:45:22 +0530

    Update FSM:One_hot_logic_equations.v

--- Building_Larger_Circuits/FSM:One_hot_logic_equations.v
+++ Building_Larger_Circuits/FSM:One_hot_logic_equations.v
@@ -11,14 +11,9 @@ module top_module(
     output done,
     output counting,
     output shift_ena
-); //
-
-    // You may use these parameters to access state bits using e.g., state[B2] instead of state[6].
+); 
     parameter S=0, S1=1, S11=2, S110=3, B0=4, B1=5, B2=6, B3=7, Count=8, Wait=9;
-
-    // assign B3_next = ...;
     assign B3_next=state[B2];
-    // assign S_next = ...;
     assign S_next=(state[S1]&(~d))|(state[S110]&(~d))|(state[Wait]&ack)|(state[S]&(~d));
     assign S1_next=(state[S]&d);
     assign Count_next=state[B3]|(state[Count]&(~done_counting));
@@ -26,6 +21,5 @@ module top_module(
     assign done=(state[Wait]);
     assign counting=(state[Count]);
     assign shift_ena=state[B0]|state[B1]|state[B2]|state[B3];
-    // etc.
 
 endmodule
